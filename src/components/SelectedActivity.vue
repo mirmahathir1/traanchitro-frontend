b907088635880f15534d9ece4e99713bbeec892f
Mahathir <mirmahathir1@gmail.com>
2020-04-18 18:13:22 +0600

    first commit

---
+++ src/components/SelectedActivity.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "SelectedActivity"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card class="mx-auto elevation-12" max-width="500" style="top: 20vh">
+        <v-list>
+            <v-list-item>
+                <v-list-item-title>
+                    Page under maintenance...
+                </v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "HowTo"
+    }
+</script>
+
+<style scoped>
+
+</style>
