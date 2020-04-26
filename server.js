8e2664abbb8c9e78eee7d54317db00ea23590da7
Mahathir <mirmahathir1@gmail.com>
2020-04-26 14:40:28 +0600

    changed port number

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+let PORT = process.env.PORT || 5000;
+
 //CONFIGURATION TO SERVE VUEJS APP------START
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
@@ -36,6 +38,6 @@ app.post('/test2',function (req,res) {
     res.send({name: "Mahathir"});
 });
 
-app.listen(5000, function () {
-    console.log( 'Express serving on http://localhost:5000' );
+app.listen(PORT, function () {
+    console.log( 'Express serving on http://localhost:',PORT );
 });
