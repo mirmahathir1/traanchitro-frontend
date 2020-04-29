7137c62ffdaaa2113ba45aeaad49211b1c32d942
Mahathir <mirmahathir1@gmail.com>
2020-04-29 17:05:56 +0600

    Automated git testing

---
+++ upload.sh
@@ -0,0 +1,8 @@
+echo "Enter commit message: "
+read -r message
+echo "The commit message is: $message"
+npm run build
+git add .
+git commit -am "$message"
+git push origin master
+git push heroku master
